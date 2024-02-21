69aae406d7c503efbaba4a8a53f2117180501892
Milroy Fraser <milroyfraser@Milroys-Mac-mini.local>
2024-02-21 01:19:28 +0100

    top nav

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,13 +1,13 @@
-<footer class="py-10 md:py-16 bg-sky-100 bg-no-repeat bg-right-bottom" style="background-image: url('/img/me-transparent-bg.png')">
+<footer class="py-10 md:py-16 bg-sky-100 bg-no-repeat bg-right-bottom border-b-4 border-sky-900" style="background-image: url('/img/me-transparent-bg.png')">
     <div class="container max-w-screen-xl mx-auto px-4">
         <div class="text-center">
-            <h1 class="font-medium text-gray-700 text-4xl md:text-5xl mb-5">Thank you!</h1>
-            <p class="text-gray-500 mb-20">
+            <h1 class="font-medium text-sky-900 text-4xl md:text-5xl mb-5">Thank you!</h1>
+            <p class="text-gray-600 mb-20">
                 If you are here, There is something special I would like to share with you.
                 <x-link url="#">Interested?</x-link>
             </p>
             <x-follow-me />
-            <p class="text-gray-500 text-xs mt-20">Copyright © {{ now()->format('Y') }} Milroy Fraser.</p>
+            <p class="text-gray-600 text-xs mt-20">Copyright © {{ now()->format('Y') }} Milroy Fraser.</p>
             <p class="text-sky-900 text-xs mt-2">
                 <small>
                     <a href="#">{{ now()->diffInDays('2031-01-21 23:59:59') }}</a>
--- resources/views/components/recommendation-card.blade.php
+++ resources/views/components/recommendation-card.blade.php
@@ -4,10 +4,10 @@
             <img alt="testimonial" src="{{ $recommendation['image'] }}" class="w-16 h-16 rounded-full flex-shrink-0 object-cover object-center border-4 border-sky-100">
             <span class="flex-grow flex flex-col pl-4">
                 <span class="font-semibold text-sky-900">{{ $recommendation['name'] }}</span>
-                <span class="text-gray-500 text-sm">{{ $recommendation['title'] }}</span>
+                <span class="text-gray-600 text-sm">{{ $recommendation['title'] }}</span>
             </span>
         </a>
-        <p class="leading-relaxed my-6">
+        <p class="leading-relaxed my-6 text-gray-800">
             {{ str($recommendation['body'])->limit(200) }} <x-link url="#">read <span>&#8690;</span></x-link>
         </p>
     </div>
